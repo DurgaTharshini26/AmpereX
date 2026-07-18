053a9b5941526f65c732eb75836f49f6d7025044
DHANALAKSHMI M <dhanalakshmi450532@gmail.com>
2026-07-18 11:10:21 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#75c79f] overflow-x-hidden selection:bg-indigo-500/30 selection:text-white">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
